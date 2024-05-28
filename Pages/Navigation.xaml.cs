0048dcfe104b240c867af6d2ee1cc8c883fce451
kdowd <kevin.dowd@gmail.com>
2024-05-28 12:25:50 +1200

    Add project files.

---
+++ Pages/Navigation.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FramesPages.Pages
+{
+    /// <summary>
+    /// Interaction logic for Navigation.xaml
+    /// </summary>
+    public partial class Navigation : Page
+    {
+        public Navigation()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var w = Application.Current.MainWindow as MainWindow;
+            var clickedElement = sender as MenuItem;
+            if (clickedElement != null)
+            {
+                object headerText = clickedElement.Header;
+
+                switch (headerText)
+                {
+
+                    case "_Close":
+
+                        Application.Current.MainWindow.Close();
+                        break;
+                    case "_Login":
+                        w?.AppFrame.Navigate(new Login());
+                        break;
+                    default: break;
+                }
+            }
+
+
+        }
+    }
+}
